620253850e8200b98e2f6edb713378cbb4eba901
kaylee182000 <trungkien40414@gmail.com>
2022-09-10 23:25:46 +0700

    new

---
+++ pages/about.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+        repudiandae quae et optio dolores a, facere, quidem neque deleniti
+        minima, beatae reiciendis! Impedit alias nostrum hic ipsum accusamus
+        veniam officia.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+        repudiandae quae et optio dolores a, facere, quidem neque deleniti
+        minima, beatae reiciendis! Impedit alias nostrum hic ipsum accusamus
+        veniam officia.
+      </p>
+    </div>
+  );
+}
---
+++ pages/coders/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AllCoders() {
+  return (
+    <div>
+        <h1>All Coders</h1>
+    </div>
+  )
+}
