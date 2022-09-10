5c5f31c043ea06a179fc9443e3cfce9d289f847c
kaylee182000 <trungkien40414@gmail.com>
2022-09-10 23:32:53 +0700

    update

---
+++ components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <div>
+        <a href="#">Coder Wiki</a>
+      </div>
+      <div>
+        <a href="#">About</a>
+        <a href="#">All Coders</a>
+      </div>
+    </nav>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>home page</h1>
-      <p>ghakhgahgahklfghahgahhakghaahghahhakhgahgfghakhahgahgahahfgh</p>
-    </div>
+    <>
+      <NavBar />
+      <div>
+        <h1>home page</h1>
+        <p>ghakhgahgahklfghahgahhakghaahghahhakhgahgfghakhahgahgahahfgh</p>
+      </div>
+      <Footer />
+    </>
   );
 }
